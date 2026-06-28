22244f1a7cf1b8bceac0665f96297430136ff526
Diego Hoyos <diego.hoyos@global66.com>
2026-06-28 12:09:25 -0500

    fix(settings): fix import

--- lib/features/settings/provider/settings_provider.dart
+++ lib/features/settings/provider/settings_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
