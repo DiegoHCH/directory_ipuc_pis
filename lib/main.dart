77395278a5926d05857a1f72501efd324f0e3b67
Diego Hoyos <diego.hoyos@global66.com>
2026-06-28 11:57:18 -0500

    fix: lock app to portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 import 'core/providers/locale_provider.dart';
 import 'core/providers/notification_listener_provider.dart';
 import 'core/providers/theme_provider.dart';
@@ -13,6 +14,7 @@ import 'l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await NotificationService.init();
   runApp(const ProviderScope(child: DirectoryApp()));
